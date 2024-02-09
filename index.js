561cef7ee8ed68a9836c6cb7a110081e5716f834
Jose A <jfacademusic@gmail.com>
2024-02-09 03:51:15 -0500

    changed root file console log

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    console.log('root folder reached')
+    console.log('root file reached')
     res.send('Hello World!')
 })
 
