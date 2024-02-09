a978921cd998b53c47e76f6ae10bb34230159a36
Jose A <jfacademusic@gmail.com>
2024-02-09 02:25:41 -0500

    seed file working

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,37 +1,49 @@
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
+import mediaData from '../data.json' assert { type: 'json' };
 
 async function main() {
-    const beyondEarth = await prisma.media.create({
-        where: {},
-        update: {},
-        create: {
-            title: 'Beyond Earth',
-            tpath: '/assets/thumbnails/beyond-earth/regular/large.jpg',
-            tpathTrending: '/assets/thumbnails/beyond-earth/trending/large.jpg',
-            year: 2019,
-            category: 'Movie',
-            rating: 'PG',
-            genre: ["Comedy", "Thriller"]
-
-        }
+  console.log(mediaData)
+  mediaData.map(async (media) => {
+    await prisma.media.create({
+      data: {
+        title: media.title,
+        year: media.year,
+        category: media.category,
+        rating: media.rating,
+        tpath: media.thumbnail.regular.large,
+        tpathTrending: media.thumbnail.regular.large,
+        genre: media.genre
+      }
     })
-    const bottomGear = await prisma.media.create({
-        where: {},
-        update: {},
-        create: {
-            title: 'Bottom Gear',
-            tpath: '/assets/thumbnails/bottom-gear/regular/large.jpg',
-            tpathTrending: '/assets/thumbnails/bottom-gear/trending/large.jpg',
-            year: 2021,
-            category: 'TV Show',
-            rating: 'PG',
-            genre: ["Romance", "SciFi", "Action"]
+  })
+  // for (let i = 0; i < mediaData.length; i++) {
 
-        }
-    })
+  // }
+  // const beyondEarth = await prisma.media.create({
+  //   data: {
+  //     title: 'Beyond Earth',
+  //     year: 2019,
+  //     category: 'Movie',
+  //     rating: 'PG',
+  //     tpath: '/assets/thumbnails/beyond-earth/regular/large.jpg',
+  //     tpathTrending: '/assets/thumbnails/beyond-earth/trending/large.jpg',
+  //     genre: ["Comedy", "Thriller"]
+  //   }
+  // })
+  // const bottomGear = await prisma.media.create({
+  //   data: {
+  //     title: 'Bottom Gear',
+  //     year: 2021,
+  //     category: 'TV Show',
+  //     rating: 'PG',
+  //     tpath: '/assets/thumbnails/bottom-gear/regular/large.jpg',
+  //     tpathTrending: '/assets/thumbnails/bottom-gear/trending/large.jpg',
+  //     genre: ["Romance", "SciFi", "Action"]
+  //   }
+  // })
 
-    console.log({ beyondEarth, bottomGear })
+  // console.log({ beyondEarth, bottomGear })
 }
 
 main()
