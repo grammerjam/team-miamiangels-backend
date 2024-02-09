a0bac5b99a9ecd5ea1aa4c97acf808bb85e9218c
Jose A <jfacademusic@gmail.com>
2024-02-09 18:03:45 -0500

    added user get and post routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getUserInfo(req, res) {
-    // console.log(req)
-    console.log("these are the params: " + req.query.userEmail)
     const userEmail = req.query.userEmail
     try {
         const user = await prisma.user.findUnique({
@@ -12,13 +10,26 @@ export async function getUserInfo(req, res) {
             }
         })
         if (!user) {
+            res.status(404).send("No such user found")
+        }
+        res.json(user);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('An error occurred while fetching media records.');
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+export async function createUser(req, res) {
+    const userEmail = req.query.userEmail
+    try {
             await prisma.user.create({
                 data: {
                     email: userEmail,
                 }
             })
-        }
-        res.json(user);
+        res.status(201).send("User successfully created");
     } catch (e) {
         console.error(e);
         res.status(500).send('An error occurred while fetching media records.');
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 // import { getAllMedia, getAllMovies, getAllTVSeries } from "../controllers/mediaController.js";
-import { getUserInfo } from '../controllers/userController.js';
+import { getUserInfo, createUser } from '../controllers/userController.js';
 const router = express.Router()
 
 router
     .route("/")
     .get(getUserInfo)
+    .post(createUser)
 
 export { router }
