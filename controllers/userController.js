9a4431f47b18ff91773798482d70827baa641871
alv-314 <alicov314@gmail.com>
2024-02-29 17:18:49 -0500

    Changed getUserBookmarks req.query from userEmail to userMail

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,7 +118,7 @@ export async function updateUserBookmarks(req, res) {
 }
 
 export async function getUserBookmarks(req, res) {
-    const userEmail = req.query.userEmail
+    const userEmail = req.query.mail
     try {
         const user = await prisma.user.findUnique({
             where: {
