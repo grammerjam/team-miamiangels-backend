05b4b1d4ac798f94bf14e5fe601d7ef7c3844cb2
Jose A <jfacademusic@gmail.com>
2024-03-20 04:53:17 -0400

    cleaned up code

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -23,19 +23,13 @@ function getMimeType(fileName) {
 
 export async function getVideoData(req, res) {
     const fileName = req.params.filename
-    // console.log(fileName)
     const range = req.headers.range
-    // const filePath = videoFileMap[fileName]
-    // const filePath = "https://miamiangelsvideos.s3.amazonaws.com/alone_-_46637_720p.mp4"
-    // console.log(fileName)
 
     const videoKey = fileName
     let s3Params = {
         Key: videoKey,
         Bucket: bucketParams.Bucket
     };
-
-    // console.log("this the range: " + range)
 
     s3.headObject(s3Params, function (err, data) {
         if (err) {
@@ -45,8 +39,6 @@ export async function getVideoData(req, res) {
         var stream = s3.getObject(s3Params).createReadStream();
 
         stream.on('error', function error(err) {
-            //continue to the next middlewares
-            // console.log('i hit an error')
             return res.status(404).send(err)
         });
 
@@ -56,19 +48,12 @@ export async function getVideoData(req, res) {
         }
 
         if (range && parts[0] > 0) {
-            // console.log('the range of this call is: ' + range)
-            // const parts = range.replace(/bytes=/, '').split('-')
-            // console.log("this is the parts: " + parts[0])
             const start = parseInt(parts[0], 10);
-            // console.log("starting at" + start)
             const end = parts[1] ? parseInt(parts[1], 10) : data.ContentLength - 1
-            // console.log("ending at " + end)
             const chunksize = end - start + 1
-
 
             res.set('Content-Type', getMimeType(videoKey));
             res.set('Content-Length', chunksize);
-            // console.log("this chunk size is: " + chunksize)
             res.set('Last-Modified', data.LastModified);
             res.set('ETag', data.ETag);
             res.set('Content-Range', `bytes ${start} - ${end}/${data.ContentLength}`)
@@ -76,13 +61,10 @@ export async function getVideoData(req, res) {
             stream.on('end', () => {
                 // console.log('Served by Amazon S3: ' + videoKey);
             });
-
             stream.pipe(res);
         } else {
-            // console.log('no range set')
             res.set('Content-Type', getMimeType(videoKey));
             res.set('Content-Length', data.ContentLength);
-            // console.log("this data length is: " + data.ContentLength)
             res.set('Last-Modified', data.LastModified);
             res.set('ETag', data.ETag);
 
@@ -92,6 +74,5 @@ export async function getVideoData(req, res) {
 
             stream.pipe(res);
         }
-
     })
 }
